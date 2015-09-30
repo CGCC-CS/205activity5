3064ab75b90ac97a28dd9e707041ebf0029ee1a3
wadehuber <wadehuber@gmail.com>
2015-09-29 23:18:45 -0700

    Updated driver to create 2 identical task lists.

--- TaskList/src/Activity5.java
+++ TaskList/src/Activity5.java
@@ -3,16 +3,24 @@ import tasks.*;
 
 public class Activity5 {
 	public static void main(String[] args) {
-		TaskList list = new ThreeTasks();
+		ThreeTasks list = new ThreeTasks();
 	
 		/* Makes 2 identical task list - one of each type of task list */
-		list = new ThreeTasks();
-	
-		list.addTask(new Task("Install Eclipse upgrade", 4));
-		list.addTask(new Task("Do Project 4", 8));
-		list.addTask(new Task("Learn about lists", 7));
+		TaskList list1 = new ThreeTasks();
+		list1.addTask(new Task("Install Eclipse upgrade", 4));
+		list1.addTask(new Task("Do Project 4", 8));
+		list1.addTask(new Task("Learn about lists", 7));
 		
-		list.printTasks();
+		System.out.println("List 1:");
+		list1.printTasks();
+
+		ArrayTasks list2 = new ArrayTasks(3);
+		list2.addTask(new Task("Install Eclipse upgrade", 4));
+		list2.addTask(new Task("Do Project 4", 8));
+		list2.addTask(new Task("Learn about lists", 7));
+		
+		System.out.println("\nList 2:");
+		list2.printTasks();
 	}
 
 }
