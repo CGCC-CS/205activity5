755b8bbae456d63545d5e08c2506a8b76556716e
wadehuber <wadehuber@gmail.com>
2015-12-21 13:39:20 -0700

    Made array of TaskLists

--- TaskList/src/Activity5.java
+++ TaskList/src/Activity5.java
@@ -3,24 +3,24 @@ import tasks.*;
 
 public class Activity5 {
 	public static void main(String[] args) {
-		ThreeTasks list = new ThreeTasks();
-	
-		/* Makes 2 identical task list - one of each type of task list */
-		TaskList list1 = new ThreeTasks();
-		list1.addTask(new Task("Install Eclipse upgrade", 4));
-		list1.addTask(new Task("Do Project 4", 8));
-		list1.addTask(new Task("Learn about lists", 7));
+		final int NUMLISTS = 2;
 		
-		System.out.println("List 1:");
-		list1.printTasks();
-
-		ArrayTasks list2 = new ArrayTasks(3);
-		list2.addTask(new Task("Install Eclipse upgrade", 4));
-		list2.addTask(new Task("Do Project 4", 8));
-		list2.addTask(new Task("Learn about lists", 7));
+		/* Makes one of each type of task list */
+		TaskList[] list = new TaskList[NUMLISTS];
+		list[0] = new ThreeTasks();
+		list[1] = new ArrayTasks(3);
 		
-		System.out.println("\nList 2:");
-		list2.printTasks();
+		for (int ii=0;ii<list.length;ii++) {
+			list[ii].addTask(new Task("Install Eclipse upgrade", 4));
+			list[ii].addTask(new Task("Do Project 4", 8));
+			list[ii].addTask(new Task("Learn about lists", 7));
+		}
+		
+		for (int ii=0;ii<list.length;ii++) {
+			System.out.println("List " + (ii + 1) + ":");
+			list[ii].printTasks();
+			System.out.println();
+		}
 	}
 
 }
